6c19ed9772bbb066ff1f11230aa8f4e0bc72f084
xilu.ll <xilu.ll@raycloud.com>
2022-05-16 17:05:43 +0800

    init vue3-admin

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "pliugin:vue/vue3-recommended",
+    "prettier",
+    "prettier/vue"
+  ],
+  rules: {
+    'vue/no-unused-vars': 'error',
+    'vue/no-multiple-template': 'off'
+  }
+}
